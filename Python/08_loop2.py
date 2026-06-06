7e9c718fada7c2cccac27c240bef50a4482d70e7
Khushi Kumari <khushi16bce@gmail.com>
2026-06-06 12:49:48 +0530

    Add loop examples in 08_loop2.py
    Add examples of loops in Python, including while and for loops.

---
+++ Python/08_loop2.py
@@ -0,0 +1,47 @@
+
+# i = 0
+# while i <= 5:
+#  if(i==3):
+#   i+=1 
+#   continue #skip
+#  print(i)
+#  i += 1
+
+# # ------ FOR LOOP -----
+
+#  tup = (1,2,3,4,5.6)   
+#  for num in tup:
+#   print(num)
+
+# str = "khushi"
+# for chr in str:
+#  print(chr)
+# else:
+#  print("end")
+
+# nums = (1,6,87,5,78,34,6)
+# x = 6
+# i = 0
+# for el in nums:
+#  if(el == x):
+#   print("no. found at idx",i)
+#  i+= 1
+
+# seq = range(6)
+# for i in seq:
+#   print(i)
+
+#   for i in range(10):   #range(stop)
+#     print(i)
+
+# for i in range(2,10):   #range( start,stop)
+#     print(i)
+
+# for i in range(2,10,3):   #range(start,stop,setp(inc fron 3))
+#     print(i)
+
+for i in range(10):   #range(stop)
+
+ pass # skip
+
+print("some useful work")
