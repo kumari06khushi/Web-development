c7a306aacb1dfb019bfe55ee23cdaf18bc45b406
Khushi Kumari <khushi16bce@gmail.com>
2026-06-06 12:47:50 +0530

    Add initial implementation of set operations in Python

---
+++ Python/06_set.py
@@ -0,0 +1,62 @@
+collection = {1,3,8,3,1,5,"khushi","khushi", "piya"}
+# print(collection)
+print(type(collection))
+print(len(collection))
+
+# collection1 = set()
+
+# print(type(collection1))
+# collection1.add(1)
+# collection1.add(2)
+# collection1.add(1)
+# collection1((2,8,9))
+
+# collection1([2,8,9])  #---> set are not accept list  ,it give error
+
+# print(collection1)
+
+# set1 = {1,2,3,4}
+# set2 = {2,3,5,6}
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+# print(set1)
+# print(set2)
+
+
+#---practice questuin 
+# dict ={
+#     "table" : [" a piece of furniture", "listof fact"],
+#     "cat" : "a small  animal"
+# }
+# print(dict)
+
+
+# classroom = {'python' ,'java','c++','python','javascript','java','python','java','c++','c'}
+# print("required classroom are",len(classroom))
+
+
+# marks = {}
+
+# x = int(input("enter phy :"))
+# marks.update({"phy" : x})
+
+# x = int(input("enter phy :"))
+# marks.update({"phy" : x})
+
+# x = int(input("enter phy :"))
+# marks.update({"phy" : x})
+
+# value = {9,9.0,"8",8.0}   #--- 9,9.0  treat as same
+# print(value)
+
+value1 = {
+    ("float",9.0),
+    ("int",9)
+}
+print(value1)
+
+
+
+
+ #----NOTES-----
+#  set is unorderd list
