7ede09b46586a2561da391973ad723d16a0acab5
Khushi Kumari <khushi16bce@gmail.com>
2026-06-06 12:48:46 +0530

    Implement examples of while loops in 07_loop.py
    Added various while loop examples demonstrating different conditions and outputs.

---
+++ Python/07_loop.py
@@ -0,0 +1,61 @@
+# count = 1
+# while count <= 5: 
+#     print("hello")
+#     count += 1
+
+
+# i = 1
+# while i<=6 :  # stoping condition
+#     print("hey khushi",i)
+#     i +=1
+
+
+# j = 5
+# while j>=1:
+#     print("hey khushi", j)
+#     j-=1
+
+
+# j = 1
+# while j<=10:
+#     print(3*j)
+#     j+=1
+
+# n = int(input("enter no:"))
+# j = 1
+# while j<=10:
+#     print(n*j)
+#     j+=1
+
+
+# nums = [1,4,9,6,49,56,78,81,100]
+
+# idx = 0
+# while idx < len(nums):
+#  print(nums[idx])
+#  idx +=1
+
+
+tuple = [25,16,89,6,49,56,78,81,100]
+x = 16
+i = 0
+while i < len(tuple):
+ if(tuple[i] == x):
+  print("gote the no",i)
+  break
+ else:
+  print("finding...")
+ i +=1
+
+print("end the loop")
+
+# k = 1
+# while k < 5:
+#  if(k == 3):
+#   k += 1
+#   continue #skip
+#  print(k)
+# k += 1
+  
+
+    
