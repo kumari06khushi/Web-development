d772e7e249acf2642887621429c354ac04bed5ae
Khushi Kumari <khushi16bce@gmail.com>
2026-06-06 12:51:01 +0530

    Add currency converter function

---
+++ Python/09_function.py
@@ -0,0 +1,69 @@
+#-----FUNCTIOn-------
+
+
+# def calc_sum(a,b):
+#     sum = a+b
+#     print(sum)
+#     #return sum
+
+# calc_sum(5,6)
+# calc_sum(2,3 )
+# calc_sum(1,2)
+
+
+# def cal_sum(a,b):
+#     return a + b
+
+# sum = cal_sum(1,2)
+# print(sum)
+
+
+# def calc_avg(a,b,c):
+#     sum = a+b+c
+#     avg = sum/3
+#     return print(avg)
+
+# calc_avg(1,3,5)
+
+# print("khushi",end=" ")  #sep=" "
+# print("kumari") #end="\n"
+
+
+# def cal_prod(a=2 , b=4):
+#  #def cal_prod(a=2,b)   --> show error
+#  #def cal_prod(a,b=6)   ---> by defoult "a" accept 1
+#     print(a*b)
+#     return(a*b)
+
+# cal_prod(1)
+
+cities = ["delhi", "gurgaon","noida"]
+heros = ["balvir","saktiman"]
+
+# def print_len(cities):   # Yaha cities real cities list nahi hai  Ye sirf ek temporary naam (box) hai
+
+#  print(len(cities))
+
+# print_len(cities)
+# print_len(heros)
+
+
+# def pint_list(list):
+#     for item in list:
+#         print(item,end=" ")
+
+
+# pint_list(cities)
+
+
+def converter(used_val):
+    inr_val = used_val*83
+    print(used_val,"USD=", inr_val, "INR")
+
+converter(10)
+
+
+#-- notes-----#
+
+
+# end=" "  --> seprate word
