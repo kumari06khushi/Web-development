730efc10e90fb1b6f4e1cf54af6955388fb72b94
Khushi Kumari <khushi16bce@gmail.com>
2026-06-06 12:45:19 +0530

    Add string manipulation and conditional examples

---
+++ Python/03_string.py
@@ -0,0 +1,86 @@
+str1 = "khushi" 
+str2 = "kumari"
+
+# print(len(str1))
+# print(str1+str2)   #concatenation--->add of two  or more string
+# ch = str1[1]
+# print(str1[1])
+# print(ch)
+# print(str1[1:3])   # slice break down of string : str[start_idx:end_idx]
+
+# print(str1[1:])  #auto eccept end idx
+
+# print(str1[-4:-2])   # backward counting counting start with back side -1(end s ek phle ki value deti h)
+
+
+
+#------ string Function -------
+
+# str = "i am  a girl"
+# sttrr = "happy"
+# print(str.endswith("rl"))  #---> true
+# str = str.capitalize()   #---> original modify
+# # print(str.capitalize()) #---> but original not change
+# print(str)
+# print(str.replace("a", "o"))  # ----> first jishe replace krna h,  old then new word ya string  jisashe replace krna h
+
+# print(sttrr.find("h"))
+#print(sttrr.count("p"))
+
+# name = input("enter your name :")
+# print(" length of name:",len(name))
+
+
+
+
+#-------conditional statement------#
+
+age = 88
+  # if(age>=18):      # (1 type) we can also write if(True): it is also if condition in python. if()  condition k hamesha check kregi   
+#     print("can vote")
+#     print("can drive")
+
+
+if(age>=18):           # (2 type) 
+  if(age>=80):
+  
+    print("can drive")
+else:
+ print("can  vote")
+
+light = "pink"
+
+# if(light=="red"):  # (3 type)
+#     print("stop")
+# elif(light=="green"):
+#     print("you can go")
+# elif(light == "yellow"):     #  ye sirf tb check hogi jb if()  k condition glt hogi  or  multiple times print k print  v kr skte h and multiple time elif k use v kr skte h
+#     print("look")
+
+# else:
+#     print("light is broken")    # ishe sirf ek bar use kr skte h ,ishe koi condition nhi likh skte
+
+
+# marks = 70
+
+# if(marks >= 90):
+#     grade = "A"
+# elif(marks >=80):
+#     grade = "B"
+# elif(marks >=70):
+#     grade = "C"
+# else:
+#     grade = "D"
+    
+
+# print("grade of the student are--> ",grade)
+
+
+
+
+
+
+
+#-----NOTES-------
+
+# In python  indentation is very important (means: prpper spacing)
