0b8127eee040d5a5947ef1ef5fd4605bd063c471
Khushi Kumari <khushi16bce@gmail.com>
2026-06-06 12:42:38 +0530

    Create 01_basic.py

---
+++ Python/01_basic.py
@@ -0,0 +1,27 @@
+name = "khushi"
+age = 18
+age2 = age  # right side value save in left side
+price = 20.00
+#print ("my name is :", name) 
+# print(age2)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+#---logical oprater 
+
+
+a = 30
+b = 10
+# print(not True)
+# print(not False)
+# print(not(a>b))
+
+
+#---NOTES-----
+#---data type------
+# integer
+# string
+# floar
+# None 
+# Boolean
