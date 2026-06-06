f4a8346ad3e01a157463c77f00db9ccce46e4d88
Khushi Kumari <khushi16bce@gmail.com>
2026-06-06 12:44:07 +0530

    Implement type conversion and user input in 02_basic.py
    Add basic type conversion and user input examples.

---
+++ Python/02_basic.py
@@ -0,0 +1,26 @@
+#-----Type conversion
+
+a = 2
+b = 4.6
+sum = a+b
+#print(sum)
+
+d = int("6")  # covert string to int
+#print(a+d)
+
+#----enter input through user
+
+
+#name = input("enter your name:")
+#print("welcome",name)
+
+# age =input("enter your age")
+# age = int(input("enter your age"))  #--> convert str into int
+# print("your age is",age)
+# print(type(age))   #----> input value in python show 'string'
+
+
+first = int(input("enter first no"))
+sec = int(input("enter sec no"))
+first += sec
+# print("sum of no are" , first )
