a8b38ef47e56e871901bbf4dcbc086a78b2ce4dc
Khushi Kumari <khushi16bce@gmail.com>
2026-07-03 10:59:18 +0530

    app file of day 3.2

---
+++ React_js/App.jsx
@@ -0,0 +1,14 @@
+
+
+   export default function App(){ //Same name of file
+    return (
+        <h1> Hello coder Army</h1>
+    )
+};
+
+//name export
+  export function Khushi(){
+return(
+    <h2>who are you </h2>
+)
+ }
