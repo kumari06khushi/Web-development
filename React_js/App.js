5566bb4f09a34f4024a253b099f1e672081ffd47
Khushi Kumari <khushi16bce@gmail.com>
2026-07-03 10:54:27 +0530

    app file  is day 3.1

--- React_js/App.js
+++ React_js/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const element = <h1> Hello coder army</h1>
+const element = <h1> Hello khushi </h1>
  //React.createElement("h1", null, "Hello coder army")
  ReactDOM.createElement(document.getElementById('root'))
  root.render(element);
